f0388e4e9354ca1a65281d564143db5d30dfd927
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 23:57:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,13 +13,14 @@ def get_shallow_data(data)
     index += 1
   end
   
-  pp hash
+  return hash
 end
 
 def get_deep_data(data)
+  hash = get_shallow_data(data)
   
 end
 
 def nyc_pigeon_organizer(data)
-  get_shallow_data(data)
+  get_deep_data_data(data)
 end
