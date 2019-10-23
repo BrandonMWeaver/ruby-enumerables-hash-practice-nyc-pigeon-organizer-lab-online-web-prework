30d0874a8097af1e82537c1a6caffe0c77fbdde2
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 23:45:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,7 @@ def get_shallow_data(data)
   hash = {}
   
   data[:gender].collect do |datum|
-    datum[:male].collect do |male|
-      hash[male] = {}
-    end
-    datum[:female].collect do |female|
-      hash[female] = {}
-    end
+  
   end
   
   pp hash
