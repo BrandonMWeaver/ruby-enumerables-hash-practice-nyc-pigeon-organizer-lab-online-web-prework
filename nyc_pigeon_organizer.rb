04df837adf59e45a84895d6066ad28d89cf7b099
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 23:32:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,14 @@ def nyc_pigeon_organizer(data)
   pp data
   
   data.collect do |datum|
-    
+    if datum == :gender
+      
+      index = 0
+      while index < datum[:male] do
+        hash[datum[:male][index]] = {}
+      end
+      
+    end
   end
   
 end
