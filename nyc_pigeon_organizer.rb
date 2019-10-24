a1387819e566436496952392e56efedbfb3ac9f1
Brandon Weaver <a7xncob@gmail.com>
2019-10-24 00:22:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  
+  index = 0
+  while index < data[:gender][:male].size do
+    hash[data[:gender][:male][index]] = { gender: ["male"], color: [], lives: [] }
+    index += 1
+  end
+  
+  index = 0
+  while index < data[:gender][:female].size do
+    hash[data[:gender][:female][index]] = { gender: ["female"], color: [], lives: [] }
+    index += 1
+  end
+  
+  data[:color].each do |color, pigeons|
+    
+    index = 0
+    while index < pigeons.size do
+      hash[pigeons[index]][:color] << "#{color}"
+      index += 1
+    end
+  end
+  
+  data[:lives].each do |lives, pigeons|
+    
+    index = 0
+    while index < pigeons.size do
+      hash[pigeons[index]][:lives] << "#{lives}"
+      index += 1
+    end
+  end
+  
+  pp hash
+  return hash
 end
